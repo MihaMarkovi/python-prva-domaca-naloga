968dd0a14471c2cd712353719190db2da88d2439
Miha Markovic <miha0552@gmail.com>
2016-10-18 18:27:59 +0200

    prva domača naloga za python

---
+++ main.py
@@ -0,0 +1,109 @@
+
+
+while True:
+    user_number = raw_input("Vpisite stevilko od 0 do 5 : ")
+    
+    if user_number == "3":
+        print("Bravo pojdi na naslednjo stopnjo!")
+        
+        while True:
+            
+              user_number = raw_input("Vpisi stevilko od 0 do 10 : ")
+              
+              if user_number == "8":
+                print("Bravo, kar tako naprej!")
+                
+                while True:
+                    user_number = raw_input("Vpisi stevilko od 0 do 15 : ")
+                    
+                    
+                    if user_number == "13":
+                        print("Bravo, kar tako naprej!")
+                        
+                        while True:
+                            user_number = raw_input("Vpisi stevilko od 0 do 20 : ")
+                            
+                            
+                            if user_number == "17":
+                                print("Bravo, kar tako naprej!")
+                                
+                                while True:
+                                
+                                    user_number = raw_input("Vpisi stevilko od 0 do 25 : ")
+                                    
+                                    
+                                    if user_number == "22":
+                                        print("Bravo, kar tako naprej!")
+                                        
+                                        while True:
+                                        
+                                            user_number = raw_input("Vpisi stevilko od 0 do 30 : ")
+                                            
+                                            
+                                            if user_number == "23":
+                                                print("Bravo, kar tako naprej!")
+                                                
+                                                while True:
+                                                
+                                                    user_number = raw_input("Vpisi stevilko od 0 do 35 : ")
+                                                    
+                                                    
+                                                    if user_number == "30":
+                                                        print("Bravo, kar tako naprej!")
+                                                        
+                                                        while True:
+                                                        
+                                                            user_number = raw_input("Vpisi stevilko od 0 do 40 : ")
+                                                            
+                                                            
+                                                            if user_number == "40":
+                                                                print("Bravo, kar tako naprej!")
+                                                                
+                                                                while True: 
+                                                                
+                                                                    user_number = raw_input("Vpisi stevilko od 0 do 45 : ")
+                                                                    
+                                                                    
+                                                                    if user_number == "41":
+                                                                        print("Bravo, kar tako naprej!")
+                                                                        
+                                                                        while True:
+                                                                        
+                                                                            user_number = raw_input("Vpisi stevilko od 0 do 50 : ")
+                                                                            
+                                                                            
+                                                                            if user_number == "45":
+                                                                                print("Cestitamo!")
+                                                                                print("Za izhod pritisni tipki ctrl + c")
+                                                                                
+                                                                            else:
+                                                                                print("Poiskusi se enkrat")
+                                                                                
+                                                                    else:
+                                                                        print("Poiskusi se enkrat")
+                                                                        
+                                                            else:
+                                                                print("Poiskusi se enkrat")
+                                                                
+                                                    else:
+                                                        print("Poiskusi se enkrat")
+                                                        
+                                            else:
+                                                print("Poiskusi se enkrat")
+                                                
+                                    else:
+                                        print("Poiskusi se enkrat")
+                                        
+                            else:
+                                print("Poiskusi se enkrat")
+                                
+                    else:
+                        print("Poiskusi se enkrat")
+                        
+                       
+              else:
+                  print("Poiskusi se enkrat")
+                  
+    else:
+        print("Poiskusi se enkrat")
+        
